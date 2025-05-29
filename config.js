f93c3ffce3e997c6d84e449889a38f76bd4e4e36
anhthuongem1000505 <antr0363802029@gmail.com>
2025-05-29 18:52:31 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,20 +1,17 @@
 const CONFIG = {
     titleWeb: "Gửi cậu....",
     introTitle: 'Phuong vy',
-    introDesc: `Trái đất vốn lạ thường
-    Mà sao em cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
-    Đằng sau chữ yêu đây là thương`,
+    introDesc: `trước giờ an đã làm Pvy buồn rất nhiều nhưng hành trình 4 năm qua chúng mình vẫn có nhau, vậy vì sao khi đã trưởng thành hơn mình lại xa nhau, nếu hết thương thì tớ sẽ đổi cách yêu vì để cậu bên tớ 1 lần nữa`,
     btnIntro: '^^HiHi^^',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 🥰',
-    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Thích lắm <33',
+    title: 'AN đã thay đổi nhiều rồi, tình cảm của an cũng vậy... 🥰',
+    desc: 'nó giờ càng nhiều thêm, hãy bắt đầu lại từ những thứ nhỏ nhặt nhất nha ',
+    btnYes: '1 lần cuối cùng nha <33',
     btnNo: 'Không nha :3',
-    question: 'Trên thế giới hơn 7 tỉ người mà sao bạn lại yêu mình <3',
-    btnReply: 'Gửi cho bạn <3',
-    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Mình biết mà 🥰. Yêu bạn nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h, mình qua đón đi chơi nha.',
+    question: 'cuộc đời mình còn dài, tớ sẽ chững minh cho cậu thấy , tớ ko mỗm <3',
+    btnReply: 'Gửi cho thk chos <3',
+    reply: 'sẽ không có lần sau nữa đâu <33333333',
+    mess: 'Mình biết mà 🥰. ctay lần nữa là lúc mình cưới đó 😘😘',
+    messDesc: 'hè này an qua đón đi chơi nha.',
     btnAccept: 'Okiiiii lun <3',
     messLink: 'https://github.com/zukahai/Confess-Crush' //link mess của các bạn. VD: https://www.facebook.com/messages/t/100014188333536
 }
